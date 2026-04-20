0d25f23169a3cd7124ee90b4d59365c53e1d5edb
MiksuNy <miksunyman1000@gmail.com>
2026-04-20 15:02:06 +0300

    Improve logging a bit

--- src/log.rs
+++ src/log.rs
@@ -1,29 +1,29 @@
 #[macro_export]
 macro_rules! log_info {
     ($x:expr) => {
-        eprint!("\n\x1B[38;5;14m[INFO]\x1B[0m {}", $x);
+        eprint!("\n\x1B[0m[{}] \x1B[38;5;14m[INFO]\x1B[0m {}", chrono::Local::now().time().format("%H:%M:%S%.3f"), $x);
     };
     ($($x:expr),*) => {
-        eprint!("\n\x1B[38;5;14m[INFO]\x1B[0m {}", format!($($x),*));
+        eprint!("\n\x1B[0m[{}] \x1B[38;5;14m[INFO]\x1B[0m {}", chrono::Local::now().time().format("%H:%M:%S%.3f"), format!($($x),*));
     };
 }
 
 #[macro_export]
 macro_rules! log_warning {
     ($x:expr) => {
-        eprint!("\n\x1B[38;5;226m[WARNING]\x1B[0m {}", $x);
+        eprint!("\n\x1B[0m[{}] \x1B[38;5;226m[WARNING]\x1B[0m {}", chrono::Local::now().time().format("%H:%M:%S%.3f"), $x);
     };
     ($($x:expr),*) => {
-        eprint!("\n\x1B[38;5;226m[WARNING]\x1B[0m {}", format!($($x),*));
+        eprint!("\n\x1B[0m[{}] \x1B[38;5;226m[WARNING]\x1B[0m {}", chrono::Local::now().time().format("%H:%M:%S%.3f"), format!($($x),*));
     };
 }
 
 #[macro_export]
 macro_rules! log_error {
     ($x:expr) => {
-        eprint!("\n\x1B[38;5;196m[ERROR]\x1B[0m {}", $x);
+        eprint!("\n\x1B[0m[{}] \x1B[38;5;196m[ERROR]\x1B[0m {}", chrono::Local::now().time().format("%H:%M:%S%.3f"), $x);
     };
     ($($x:expr),*) => {
-        eprint!("\n\x1B[38;5;196m[ERROR]\x1B[0m {}", format!($($x),*));
+        eprint!("\n\x1B[0m[{}] \x1B[38;5;196m[ERROR]\x1B[0m {}", chrono::Local::now().time().format("%H:%M:%S%.3f"), format!($($x),*));
     };
 }
